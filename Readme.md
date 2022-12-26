2edc4a99192cd1d2a2f2719c54b2455f8ae94291
AdelKudryashev <adek-p@yandex.ru>
2022-12-26 14:25:40 +0300

    First version

---
+++ Readme.md
@@ -0,0 +1,53 @@
+This script is designed to automatically install the Strongswan daemon. Use Ubuntu 20.04. On it this script is tested and works well.
+
+<h2>Installation.</h2>
+Before running the script, find out your network interface. Use command
+<font color="red">ip route show default</font>
+to find out your network interface.
+After applying the command, you will see output like
+<font color="red">Output
+default via your_server_ip dev ens3 proto static</font>. After <font color="red">dev</font> there will be the required interfaces, in this case it is <font color="red">ens3</font>. Insert your interface in the script, use the search for this.
+
+Use the command <font color="red">sh startshan.sh</font> to run the script
+
+At the end of the script installation, you will need to confirm the inclusion of the ufw firewall twice. just press <font color="red">y</font> and <font color="red">Enter</font>.
+
+</h2>Usage.</h2>
+You will need to add a username and password.
+Edit the sudo <font color="red">/etc/ipsec.secrets</font> file. Inside the file there is a template for how to add a new user.
+After saving the file, restart Strongswan using the command.
+sudo systemctl restart strongswan-starter
+
+You can get a certificate with the help of the command
+<font color="red">cat /etc/ipsec.d/cacerts/ca-cert.pem</font>
+Keep the text between
+<font color="red">-----BEGIN CERTIFICATE----- and -----END CERTIFICATE-----</font>
+to the local computer including these two lines in a <font color="red">.pem</font> file
+
+
+
+
+Этот скрипт предназначен для автоматической установки демона Strongswan. Используйте Ubuntu 20.04, на ней этото скипт протестирован и работает хорошо.
+
+<h2>Установка.</h2>
+Перед запуском скрипта узнайте свой сетевой интерфейс. Используйте команду 
+<font color="red">ip route show default</font> чтобы узнать свой сетевой интерфейс.
+После применения команды вы увидите вывод вида
+<font color="red">Output
+default via your_server_ip dev ens3  proto static</font>. После <font color="red">dev</font> будет нужный интерфейы, в данном случае это <font color="red">ens3</font>. Вставьте в скрипте ваш интерфейс, используйте поиск для этого.
+
+Используйте команду <font color="red">sh startshan.sh</font>  для запуска скрипта.
+
+В конце установки скрипта два раза нужно будет подтвердить включение брандмауэра ufw. просто нажмите <font color="red">y</font> и <font color="red">Enter</font>.
+
+<h2>Использование.</h2>
+Вам нужно будет добавить логин и пароль пользователя.
+Отредактируйте файл <font color="red">sudo /etc/ipsec.secrets</font>. Внутри файла есть шаблон как добавлять нового пользователя.
+После сохранения файла перезагрузите Strongswan используя команду.
+<font color="red">sudo systemctl restart strongswan-starter</font>
+
+Получить сертефикат можно с помошью команды
+<font color="red">cat /etc/ipsec.d/cacerts/ca-cert.pem</font>
+Сохраните текст, заключенный между
+<font color="red">-----BEGIN CERTIFICATE-----</font> и <font color="red">-----END CERTIFICATE-----</font>
+на локальный компьютер включая эти две строки в файл с расширением <font color="red">.pem</font>
---
+++ startswan.sh
@@ -0,0 +1,169 @@
+#You must change the ens3 interface to your own in this script in /etc/ufw/before.rules in three places
+#Вы должны изменить интерфейс ens3 на свой в этом скрипте в /etc/ufw/before.rules в трех местах
+#!/bin/bash
+apt update
+myip=$(wget -qO - eth0.me)
+apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins -y
+mkdir -p ~/pki/cacerts
+mkdir -p ~/pki/certs
+mkdir -p ~/pki/private
+
+chmod 700 ~/pki
+
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+
+pki --self --ca --lifetime 3650 --in ~/pki/private/ca-key.pem \
+--type rsa --dn "CN=VPN root CA" --outform pem > ~/pki/cacerts/ca-cert.pem
+
+pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
+
+pki --pub --in ~/pki/private/server-key.pem --type rsa \
+    | pki --issue --lifetime 1825 \
+        --cacert ~/pki/cacerts/ca-cert.pem \
+        --cakey ~/pki/private/ca-key.pem \
+        --dn "CN=$myip" --san @$myip --san $myip \
+        --flag serverAuth --flag ikeIntermediate --outform pem \
+    >  ~/pki/certs/server-cert.pem
+
+cp -r ~/pki/* /etc/ipsec.d/
+
+mv /etc/ipsec.conf{,.original} #Создаем резервную копию файла настроек
+
+cat << EOF > /etc/ipsec.conf
+config setup
+    charondebug="ike 1, knl 1, cfg 0"
+    uniqueids=no
+
+conn ikev2-vpn
+    auto=add
+    compress=no
+    type=tunnel
+    keyexchange=ikev2
+    fragmentation=yes
+    forceencaps=yes
+    dpdaction=clear
+    dpddelay=300s
+    rekey=no
+    left=%any
+    leftid=$myip
+    leftcert=server-cert.pem
+    leftsendcert=always
+    leftsubnet=0.0.0.0/0
+    right=%any
+    rightid=%any
+    rightauth=eap-mschapv2
+    rightsourceip=10.10.10.0/24
+    rightdns=8.8.8.8,8.8.4.4
+    rightsendcert=never
+    eap_identity=%identity
+    ike=chacha20poly1305-sha512-curve25519-prfsha512,aes256gcm16-sha384-prfsha384-ecp384,aes256-sha1-modp1024,aes128-sha1-modp1024,3des-sha1-modp1024!
+    esp=chacha20poly1305-sha512,aes256gcm16-ecp384,aes256-sha256,aes256-sha1,3des-sha1!
+EOF
+
+cat << EOF > /etc/ipsec.secrets
+: RSA "server-key.pem"
+# your_username : EAP "your_password" - use this format for create new user
+# sudo systemctl restart strongswan-starter - for the changes to take effect, after adding a new user, close the file and restart the server with this command
+EOF
+
+
+ufw allow OpenSSH
+ufw enable
+ufw allow 500,4500/udp
+
+cat << EOF > /etc/ufw/before.rules
+*nat
+-A POSTROUTING -s 10.10.10.0/24 -o ens3 -m policy --pol ipsec --dir out -j ACCEPT #change ens3 interface to yours
+-A POSTROUTING -s 10.10.10.0/24 -o ens3 -j MASQUERADE                             #change ens3 interface to yours
+COMMIT
+
+*mangle
+-A FORWARD --match policy --pol ipsec --dir in -s 10.10.10.0/24 -o ens3 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360 #change ens3 interface to yours
+COMMIT
+#
+# rules.before
+#
+# Rules that should be run before the ufw command line added rules. Custom
+# rules should be added to one of these chains:
+#   ufw-before-input
+#   ufw-before-output
+#   ufw-before-forward
+#
+
+# Don't delete these required lines, otherwise there will be errors
+*filter
+:ufw-before-input - [0:0]
+:ufw-before-output - [0:0]
+:ufw-before-forward - [0:0]
+:ufw-not-local - [0:0]
+# End required lines
+-A ufw-before-forward --match policy --pol ipsec --dir in --proto esp -s 10.10.10.0/24 -j ACCEPT
+-A ufw-before-forward --match policy --pol ipsec --dir out --proto esp -d 10.10.10.0/24 -j ACCEPT
+
+# allow all on loopback
+-A ufw-before-input -i lo -j ACCEPT
+-A ufw-before-output -o lo -j ACCEPT
+
+# quickly process packets for which we already have a connection
+-A ufw-before-input -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-output -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+-A ufw-before-forward -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+
+# drop INVALID packets (logs these in loglevel medium and higher)
+-A ufw-before-input -m conntrack --ctstate INVALID -j ufw-logging-deny
+-A ufw-before-input -m conntrack --ctstate INVALID -j DROP
+
+# ok icmp codes for INPUT
+-A ufw-before-input -p icmp --icmp-type destination-unreachable -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type time-exceeded -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type parameter-problem -j ACCEPT
+-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT
+
+# ok icmp code for FORWARD
+-A ufw-before-forward -p icmp --icmp-type destination-unreachable -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type time-exceeded -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type parameter-problem -j ACCEPT
+-A ufw-before-forward -p icmp --icmp-type echo-request -j ACCEPT
+
+# allow dhcp client to work
+-A ufw-before-input -p udp --sport 67 --dport 68 -j ACCEPT
+
+#
+# ufw-not-local
+#
+-A ufw-before-input -j ufw-not-local
+
+# if LOCAL, RETURN
+-A ufw-not-local -m addrtype --dst-type LOCAL -j RETURN
+
+# if MULTICAST, RETURN
+-A ufw-not-local -m addrtype --dst-type MULTICAST -j RETURN
+
+# if BROADCAST, RETURN
+-A ufw-not-local -m addrtype --dst-type BROADCAST -j RETURN
+
+# all other non-local packets are dropped
+-A ufw-not-local -m limit --limit 3/min --limit-burst 10 -j ufw-logging-deny
+-A ufw-not-local -j DROP
+
+# allow MULTICAST mDNS for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 224.0.0.251 --dport 5353 -j ACCEPT
+
+# allow MULTICAST UPnP for service discovery (be sure the MULTICAST line above
+# is uncommented)
+-A ufw-before-input -p udp -d 239.255.255.250 --dport 1900 -j ACCEPT
+
+# don't delete the 'COMMIT' line or these rules won't be processed
+COMMIT
+EOF
+
+cat << EOF >> /etc/ufw/sysctl.conf
+net/ipv4/ip_forward=1
+net/ipv4/conf/all/accept_redirects=0
+net/ipv4/conf/all/send_redirects=0
+net/ipv4/ip_no_pmtu_disc=1
+EOF
+
+ufw disable
+ufw enable
